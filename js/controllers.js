05a94a880980343fea5e8b5f7b2f222c7dc68624
PJ <yorko9999@gmail.com>
2015-04-23 14:43:18 +0100

    Fixed a problem with the detail controller

--- js/controllers.js
+++ js/controllers.js
@@ -15,14 +15,12 @@ politicsApp
 	});
 }])
 
-.controller('DetailCtrl', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http) {
-	$scope.constituency_name = $stateParams.constituencyName;
-
-	$http.get('constituencies.json').success(function(data) {
-		$scope.constituency = data.filter(function(entry) {
-			return entry.constituency_name = $scope.constituency_name;
-		})[0];
+.controller('DetailCtrl', ['$scope', '$stateParams', 'PoliticsService', function($scope, $stateParams, PoliticsService) {
+	PoliticsService.detail($stateParams.constituencyName, function(response) {
+		$scope.constituency = response;
 	});
+	
+
 }])
 
 .controller('GameCtrl', ['$scope', 'PoliticsService', function($scope, PoliticsService) {
